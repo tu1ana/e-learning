f08220967b6e6c1dc0b92b2a5f78a007b4c8bb29
Tuyana Chirninova <hikkonki@gmail.com>
2023-11-25 17:07:21 +0300

    subscription paths

--- course/urls.py
+++ course/urls.py
@@ -10,11 +10,15 @@ router = SimpleRouter()
 router.register(r'courses', CourseViewSet, basename='courses')
 
 urlpatterns = [
-    path('lessons/', LessonListAPIView.as_view()),
+    path('lessons/', LessonListAPIView.as_view(), name='lesson_list'),
     path('lessons/<int:pk>/', LessonRetrieveAPIView.as_view()),
-    path('lessons/create/', LessonCreateAPIView.as_view()),
-    path('lessons/update/<int:pk>/', LessonUpdateAPIView.as_view()),
-    path('lessons/delete/<int:pk>/', LessonDestroyAPIView.as_view()),
+    path('lessons/create/', LessonCreateAPIView.as_view(), name='lesson_create'),
+    path('lessons/update/<int:pk>/', LessonUpdateAPIView.as_view(),  name='lesson_update'),
+    path('lessons/delete/<int:pk>/', LessonDestroyAPIView.as_view(), name='lesson_delete'),
 
-    path('payment/', PaymentListAPIView.as_view())
+    path('payment/', PaymentListAPIView.as_view()),
+
+    path('subscription/create/', SubscriptionCreateAPIView.as_view()),
+    path('subscription/delete/<int:pk>/', SubscriptionDestroyAPIView.as_view())
+
 ] + router.urls
